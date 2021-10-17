ed721aaf345a78a7a0df7a14110247ea3ebef2dd
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:54:28 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/aiueo-typing/",
   "/aiueo-typing/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/aiueo-typing/keyboard.mp3",
   "/aiueo-typing/favicon/original.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/index.js",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/css/index.css",
 ];
