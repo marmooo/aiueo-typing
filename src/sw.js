db29f54ef6d51b7fd28de3a61f3bfa8ce000d7dd
marmooo <marmooo@users.noreply.github.com>
2022-05-07 10:18:59 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-16 12:55';
+var CACHE_NAME = '2022-05-07 10:20';
 var urlsToCache = [
   "/aiueo-typing/",
   "/aiueo-typing/index.js",
@@ -7,7 +7,7 @@ var urlsToCache = [
   "/aiueo-typing/mp3/correct.mp3",
   "/aiueo-typing/mp3/end.mp3",
   "/aiueo-typing/mp3/keyboard.mp3",
-  "/aiueo-typing/favicon/original.svg",
+  "/aiueo-typing/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
