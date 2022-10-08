945b5adc8039f2425915da230ef3b80922dc4b86
marmooo <marmooo@users.noreply.github.com>
2022-10-08 11:15:46 +0900

    bump bootstrap from 5.2.1 to 5.2.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-09-14 00:00";
+var CACHE_NAME = "2022-10-08 11:03";
 var urlsToCache = [
   "/aiueo-typing/",
   "/aiueo-typing/index.js",
@@ -9,7 +9,7 @@ var urlsToCache = [
   "/aiueo-typing/mp3/keyboard.mp3",
   "/aiueo-typing/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
 ];
 
