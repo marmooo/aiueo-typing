e9085e10e3b60dbe5456f9d83028f11cd8b33f61
marmooo <marmooo@users.noreply.github.com>
2023-05-13 11:56:07 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const romaNode = document.getElementById("roma");
 const gradeOption = document.getElementById("gradeOption");
 const aa = document.getElementById("aa");
 const tmpCanvas = document.createElement("canvas");
-const aiueo = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+const aiueo =
+  "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
 const aiueoRoma =
   "a|i|u|e|o|ka,ca|ki|ku,cu|ke|ko,co|sa|si,shi,ci|su|se,ce|so|ta|ti,chi|tu,tsu|te|to|na|ni|nu|ne|no|ha|hi|hu,fu|he|ho|ma|mi|mu|me|mo|ya|yu|yo|ra|ri|ru|re|ro|wa|wo|nn,xn"
     .split("|");
@@ -30,12 +31,7 @@ let typeIndex = 0;
 let errorCount = 0;
 let normalCount = 0;
 let solveCount = 0;
-let englishVoices = [];
 let guide = true;
-let keyboardAudio, correctAudio, incorrectAudio, endAudio;
-loadAudios();
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
 const layout109 = {
   "default": [
     "q w e r t y u i o p",
@@ -54,6 +50,14 @@ const simpleKeyboard = new SimpleKeyboard.default({
     typeEventKey(input);
   },
 });
+const audioContext = new AudioContext();
+const audioBufferCache = {};
+loadAudio("end", "mp3/end.mp3");
+loadAudio("keyboard", "mp3/keyboard.mp3");
+loadAudio("correct", "mp3/correct.mp3");
+loadAudio("incorrect", "mp3/cat.mp3");
+let japaneseVoices = [];
+loadVoices();
 loadConfig();
 
 function loadConfig() {
@@ -116,52 +120,33 @@ function toggleDarkMode() {
   }
 }
 
-function playAudio(audioBuffer, volume) {
-  const audioSource = audioContext.createBufferSource();
-  audioSource.buffer = audioBuffer;
+async function playAudio(name, volume) {
+  const audioBuffer = await loadAudio(name, audioBufferCache[name]);
+  const sourceNode = audioContext.createBufferSource();
+  sourceNode.buffer = audioBuffer;
   if (volume) {
     const gainNode = audioContext.createGain();
     gainNode.gain.value = volume;
     gainNode.connect(audioContext.destination);
-    audioSource.connect(gainNode);
-    audioSource.start();
+    sourceNode.connect(gainNode);
+    sourceNode.start();
   } else {
-    audioSource.connect(audioContext.destination);
-    audioSource.start();
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start();
   }
+}
+
+async function loadAudio(name, url) {
+  if (audioBufferCache[name]) return audioBufferCache[name];
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  audioBufferCache[name] = audioBuffer;
+  return audioBuffer;
 }
 
 function unlockAudio() {
   audioContext.resume();
-}
-
-function loadAudio(url) {
-  return fetch(url)
-    .then((response) => response.arrayBuffer())
-    .then((arrayBuffer) => {
-      return new Promise((resolve, reject) => {
-        audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
-          resolve(audioBuffer);
-        }, (err) => {
-          reject(err);
-        });
-      });
-    });
-}
-
-function loadAudios() {
-  promises = [
-    loadAudio("mp3/keyboard.mp3"),
-    loadAudio("mp3/correct.mp3"),
-    loadAudio("mp3/cat.mp3"),
-    loadAudio("mp3/end.mp3"),
-  ];
-  Promise.all(promises).then((audioBuffers) => {
-    keyboardAudio = audioBuffers[0];
-    correctAudio = audioBuffers[1];
-    incorrectAudio = audioBuffers[2];
-    endAudio = audioBuffers[3];
-  });
 }
 
 function loadVoices() {
@@ -185,15 +170,14 @@ function loadVoices() {
     }
   });
   allVoicesObtained.then((voices) => {
-    englishVoices = voices.filter((voice) => voice.lang == "ja-JP");
+    japaneseVoices = voices.filter((voice) => voice.lang == "ja-JP");
   });
 }
-loadVoices();
 
 function loopVoice(text, n) {
   speechSynthesis.cancel();
   const msg = new SpeechSynthesisUtterance(text);
-  msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
+  msg.voice = japaneseVoices[Math.floor(Math.random() * japaneseVoices.length)];
   msg.lang = "ja-JP";
   for (let i = 0; i < n; i++) {
     speechSynthesis.speak(msg);
@@ -333,7 +317,7 @@ function checkTypeStyle(currNode, word, key, romaNode) {
 
 function typeNormal(currNode) {
   currNode.classList.remove("d-none");
-  playAudio(keyboardAudio);
+  playAudio("keyboard");
   currNode.style.color = "silver";
   typeIndex += 1;
   normalCount += 1;
@@ -350,7 +334,7 @@ function underlineSpace(currNode) {
 }
 
 function nextProblem() {
-  playAudio(correctAudio);
+  playAudio("correct");
   typeIndex = 0;
   solveCount += 1;
   typable();
@@ -421,7 +405,7 @@ function typeEventKey(key) {
         romaNode,
       );
       if (!state) {
-        playAudio(incorrectAudio, 0.3);
+        playAudio("incorrect", 0.3);
         errorCount += 1;
       }
     }
@@ -516,7 +500,7 @@ function typable() {
     speechSynthesis.cancel();
     clearInterval(typeTimer);
     bgm.pause();
-    playAudio(endAudio);
+    playAudio("end");
     scoring();
   } else {
     const ja = problem[solveCount];
@@ -597,7 +581,7 @@ function startTypeTimer() {
     } else {
       clearInterval(typeTimer);
       bgm.pause();
-      playAudio(endAudio);
+      playAudio("end");
       scoring();
     }
   }, 1000);
