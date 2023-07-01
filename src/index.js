277be6d9b15755442f41061d499631b4bba58d8a
marmooo <marmooo@users.noreply.github.com>
2023-07-02 00:00:47 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -62,11 +62,21 @@ loadConfig();
 
 function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
   if (localStorage.getItem("bgm") != 1) {
     document.getElementById("bgmOn").classList.add("d-none");
     document.getElementById("bgmOff").classList.remove("d-none");
+  }
+}
+
+function toggleDarkMode() {
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
+    document.documentElement.setAttribute("data-bs-theme", "light");
+  } else {
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 }
 
@@ -107,16 +117,6 @@ function toggleGuide(event) {
     guide = true;
   } else {
     guide = false;
-  }
-}
-
-function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    delete document.documentElement.dataset.theme;
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.dataset.theme = "dark";
   }
 }
 
@@ -318,7 +318,7 @@ function checkTypeStyle(currNode, word, key, romaNode) {
 function typeNormal(currNode) {
   currNode.classList.remove("d-none");
   playAudio("keyboard");
-  currNode.style.color = "silver";
+  currNode.classList.add("typed");
   typeIndex += 1;
   normalCount += 1;
 }
@@ -350,13 +350,13 @@ function removeGuide(currNode) {
       key = key.toLowerCase();
     }
     const button = simpleKeyboard.getButtonElement(key);
-    button.classList.remove("bg-info");
+    button.classList.remove("guide");
   }
   let key = currNode.textContent;
   if (key == " ") key = "{space}";
   const button = simpleKeyboard.getButtonElement(key);
   if (button) {
-    button.classList.remove("bg-info");
+    button.classList.remove("guide");
   }
 }
 
@@ -365,7 +365,7 @@ function showGuide(currNode) {
     const key = currNode.textContent;
     const button = simpleKeyboard.getButtonElement(key);
     if (button) {
-      button.classList.add("bg-info");
+      button.classList.add("guide");
     }
   }
 }
